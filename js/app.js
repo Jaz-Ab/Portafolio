172902a2d79f5fd9e8857d23d31451b742846a46
YaniraAB <arenazas2.2@hotmail.com>
2018-02-19 23:07:01 -0500

    completando portafolio a un 60%

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 $(document).ready(begin);
 
 function begin() {
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav();
+  var scroll = new SmoothScroll('a[href*="#"]', {
+  	// Selectors
+  	ignore: '[data-scroll-ignore]', // Selector for links to ignore (must be a valid CSS selector)
+  	header: null, // Selector for fixed headers (must be a valid CSS selector)
+  	// Speed & Easing
+  	speed: 500, // Integer. How fast to complete the scroll in milliseconds
+  	offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+  	easing: 'easeInOutCubic', // Easing pattern to use
+  	customEasing: function(time) {
+  		return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
+  	},
+  	// Callback API
+  	before: function(anchor, toggle) {}, // Callback to run before scroll
+  	after: function(anchor, toggle) {} // Callback to run after scroll
+  });
 }
